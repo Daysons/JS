c7f0c2fb7ee9c46e08a02a01f94a1d4c3d0e1448
everelegance <everelegance@daylotusdeMacBook-Pro.local>
2018-07-13 17:59:07 +0800

    引用类型 对象 数组 Date对象 RegExp对象 Function类型 基本包装类型 单体

--- index.js
+++ index.js
@@ -22,8 +22,33 @@ displayInfo({
     Name: "Joe"
 });
 
+
 var colors = ["purple","pink", "orange"];
 
 console.log(colors.toString());
-console.log(colors.join(" * "))
+console.log(colors.join(" * "));
 console.log(Array.isArray(colors.valueOf()));
+
+
+var numbers = [2,4,3,5,1,10,23,17,13,0];
+function compare(num1, num2) {
+    return num1 - num2
+}
+
+//返回新的数组 不改变原数组
+console.log(numbers.concat(colors));
+console.log(numbers.slice(2, 6));
+
+//改变原数组
+console.log(numbers.reverse());
+console.log(numbers.sort().reverse());
+console.log(numbers.sort(compare));
+console.log(numbers.sort(compare).reverse());  // [23,17,13,10,5,4,3,2,1,0]
+console.log(numbers.splice(3,4)); // 返回[10,5,4,3] 原数组[23,17,13,2,1,0]
+console.log(numbers.splice(3,0,colors)); //返回[]  原数组[23,17,13,['purple],'pink','orange',2,1,0]
+
+//forEach()没有返回值 类似于for循环
+
+
+
+
